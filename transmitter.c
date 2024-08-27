d9e1af69fbab94ea6557f332d99ffeafc4ee7428
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2024-08-27 21:00:39 +0530

    Add files via upload

--- transmitter.c
+++ transmitter.c
@@ -4,19 +4,21 @@
 #include <util/delay.h> // Include delay functions
 #include "crc32.h" // Include CRC checksum 
 
-#define CLOCK_PIN PD0  // Define CLOCK_PIN, adjust as necessary
-#define DATA_PIN PD1   // Define DATA_PIN, adjust as necessary
-#define ACK_PIN PD2
+#define CLOCK_PIN PD0  // Define CLOCK PULSE PIN
+#define DATA_PIN PD1   // Define DATA PIN for 1 and 0
+#define ACK_PIN PD2    // Define ACKNOWLEDGE PIN
 
-void transmit(void *data, unsigned int size, unsigned long bit_rate) {
+#define bit(b) (1 << (b))
+
+void transmit(unsigned char *data, unsigned int size, unsigned long bit_rate) {
 	// calculate CRC value of the data
 	crc32_t crc = crc32(data, size);
 	
 	// transmit 32bit crc
 	for (unsigned char index = 0; index < 32; index++) {
-		(crc & (1 << (31 - index))) ? PORTD |= (1 << DATA_PIN) : PORTD &= ~ (1 << DATA_PIN);
+		(crc & bit(31 - index)) ? PORTD |= (1 << DATA_PIN) : PORTD &= ~ (1 << DATA_PIN);
 		
-		PORTD |= (1 << CLOCK_PIN);  // Set CLOCK_PIN high
+		PORTD |= bit(CLOCK_PIN);  // Set CLOCK_PIN high
 		_delay_us(1000000 / (2 * bit_rate));
 		
 		
@@ -25,12 +27,8 @@ void transmit(void *data, unsigned int size, unsigned long bit_rate) {
 	}
 	
 	// transmit 32bit data length 
-	for (unsigned char i = 0; i < 32; i++) {
-		if (size & (1 << (31 - i))) {
-			PORTD |= (1 << DATA_PIN); // Set DATA_PIN high 
-		} else {
-			PORTD &= ~(1 << DATA_PIN); // Set DATA_PIN low
-		}
+	for (unsigned char index = 0; index < 32; index++) {
+		(size & (1 << (31 - index))) ? PORTD |= (1 << DATA_PIN) : PORTD &= ~(1 << DATA_PIN);
 		
 		PORTD |= (1 << CLOCK_PIN);  // Set CLOCK_PIN high
 		_delay_us(1000000 / (2 * bit_rate));
@@ -43,12 +41,7 @@ void transmit(void *data, unsigned int size, unsigned long bit_rate) {
 	// transmit data
     for (unsigned char i = 0; i < size; i++) {
         for (unsigned char x = 0; x < 8; x++) {
-        	// Update DATA_PIN based on the bit value from data[i]
-            if ((((unsigned char*)data)[i] >> x) & 0x01) {
-                PORTD |= (1 << DATA_PIN);  // Set DATA_PIN high
-            } else {
-                PORTD &= ~(1 << DATA_PIN); // Set DATA_PIN low
-            }
+        	(data[i] & bit(7 - x)) ? PORTD |= (1 << DATA_PIN) : PORTD &= ~(1 << DATA_PIN);
             
             PORTD |= (1 << CLOCK_PIN);  // Set CLOCK_PIN high
             _delay_us(1000000 / (2 * bit_rate));              
@@ -58,14 +51,14 @@ void transmit(void *data, unsigned int size, unsigned long bit_rate) {
         }
         
         // wait for ACK pin high 
-        while (!(PIND & (1 << ACK_PIN))) {}
+        while (!(PIND & bit(ACK_PIN))) {}
     }
 }
 
 int main(void) {
     // Initialize PORTD for sending data (output mode)
-    DDRD = (1 << CLOCK_PIN) | (1 << DATA_PIN); // Set CLOCK_PIN and DATA_PIN as output
-    DDRD &= (1 << ACK_PIN); // Set ACK_PIN as input 
+    DDRD = bit(CLOCK_PIN) | bit(DATA_PIN); // Set CLOCK_PIN and DATA_PIN as output
+    DDRD &= ~ bit(ACK_PIN); // Set ACK_PIN as input 
     
     unsigned char data[] = "halloweeks";
     
