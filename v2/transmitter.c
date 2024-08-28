f8b641b3166c5185b797dc7062ea423866d8c530
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2024-08-28 18:27:38 +0530

    Update transmitter.c

--- v2/transmitter.c
+++ v2/transmitter.c
@@ -3,26 +3,23 @@
 #include <avr/io.h>  // Include AVR IO definitions
 #include <util/delay.h> // Include delay functions
 
-#define CLOCK_PIN PD0  // Define CLOCK PULSE PIN
-#define DATA_PIN PD1   // Define DATA PIN for 1 and 0
+#define CLOCK_PIN PD7  // Define CLOCK PIN
+#define DATA_PIN PD6   // Define DATA PIN
 
 #define bit(b) (1 << (b))
 
-#define WAIT_FOR_HIGH(pin) while (!(PIND & bit(pin)))
-#define WAIT_FOR_LOW(pin) while (PIND & bit(pin))
-
-void transmit(unsigned char *data, unsigned int size, unsigned long bit_rate) {
+void transmit(char *data, unsigned int size, unsigned long bit_rate) {
 	for (unsigned char index = 0; index < size * 8; index++) {
 		if (data[index / 8] & bit(7 - (index % 8))) {
-			PORTD |= (1 << DATA_PIN);
+			PORTD |= bit(DATA_PIN);
 		} else {
-			PORTD &= ~(1 << DATA_PIN);
+			PORTD &= ~bit(DATA_PIN);
 		}
 		
-		PORTD |= (1 << CLOCK_PIN);  // Set CLOCK_PIN high
+		PORTD |= bit(CLOCK_PIN);  // Set CLOCK_PIN high
 		_delay_us(1000000 / (2 * bit_rate));              
 		
-		PORTD &= ~(1 << CLOCK_PIN);  // Set CLOCK_PIN low
+		PORTD &= ~bit(CLOCK_PIN);  // Set CLOCK_PIN low
 		_delay_us(1000000 / (2 * bit_rate));
 	}
 }
