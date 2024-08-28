2578ba1ae43b19a439937c04afddd6db390c2cd3
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2024-08-28 18:29:46 +0530

    Update receiver.c

--- v2/receiver.c
+++ v2/receiver.c
@@ -2,15 +2,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define CLOCK_PIN 0  // Define CLOCK_PIN
-#define DATA_PIN 1   // Define DATA_PIN
+#define CLOCK_PIN PD7  // Define CLOCK_PIN
+#define DATA_PIN PD6   // Define DATA_PIN
 
 #define bit(b) (1 << (b))
 
 #define WAIT_FOR_HIGH(pin) while (!(PIND & bit(pin)))
 #define WAIT_FOR_LOW(pin) while (PIND & bit(pin))
 
-void recv_data(unsigned char *data, unsigned int size) {
+void recv_data(char *data, unsigned int size) {
 	for (unsigned int index = 0; index < size * 8; index++) {
 		// Wait for clock signal to go high
 		WAIT_FOR_HIGH(CLOCK_PIN);
@@ -29,9 +29,9 @@ int main(void) {
 	DDRD &= ~bit(CLOCK_PIN); // Set clock pin for input
 	DDRD &= ~bit(DATA_PIN); // Set Data pin for input
 	
-	unsigned char buffer[100] = {0}; // hold 100 byte data in memory 
+	char buffer[100] = {0}; // hold 100 byte data in memory 
 	
-	recv_data(buffer);
+	recv_data(buffer, 100);
 	
 	while (1) {}
 	return 0;
