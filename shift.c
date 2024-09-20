f6e3f85c54b3643f51ba7d44997729220867a51c
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2024-09-20 10:34:08 +0530

    Add files via upload

--- shift.c
+++ shift.c
@@ -5,33 +5,39 @@
 #define CLOCK_PIN PD6
 #define LATCH_PIN PD7
 
-void shiftOut(unsigned int data) {
-	for (unsigned char i = 0; i < 8; i++) {
-		PORTD &=~ (1 << CLOCK_PIN); // CLOCK LOW
-		
+void _sys_write_register(unsigned char data) {
+	for (unsigned char i = 8; i-->0;) {
+		// Most Significant Bit
 		if (data & (1 << i)) {
 			PORTD |= (1 << DATA_PIN); // DATA BIT 1
 		} else {
 			PORTD &=~ (1 << DATA_PIN); // DATA BIT 0
 		}
 		
+		PORTD &=~ (1 << CLOCK_PIN); // CLOCK LOW
+		_delay_us(10);
 		PORTD |= (1 << CLOCK_PIN); // CLOCK HIGH 
+		_delay_us(10);
 	}
 	
 	PORTD &=~ (1 << LATCH_PIN); // LATCH LOW
+	_delay_us(10);
 	PORTD |= (1 << LATCH_PIN); // LATCH HIGH 
+	_delay_us(10);
 }
 
+
+
 int main() {
-	// SET DATA_PIN, CLOCK_PIN, LATCH_PIN OUTPUT 
+	// SET DATA_PIN, CLOCK_PIN, LATCH_PIN AS OUTPUT 
 	DDRD |= (1 << DATA_PIN) | (1 << CLOCK_PIN) | (1 << LATCH_PIN);
 	
-	// RUN CONTINUOUSLY
+	unsigned char data = 0b00000000;
+	
 	while (1) {
-		shiftOut(0xFF); // All LEDs on
-		_delay_ms(1000); // Delay 1 Second 
-		shiftOut(0x00); // All LEDs off
-		_delay_ms(1000); // Delay 1 Second 
+		data ^= (1 << 0); // TOGGLE PIN 0
+		_sys_write_register(data); // WRITE OUTPUT IN SHIFT REGISTER 
+		_delay_ms(1000); // DELAY FOR 1 SECOND 
 	}
 	
 	return 0;
